4366bfd7e7f138d8b95266fced386b42c34b27cc
pietrzakacper <kacpietrzak@gmail.com>
2016-07-15 23:08:33 +0200

    Button's are click sensitive now

--- script.js
+++ script.js
@@ -3,39 +3,51 @@ secretWord = secretWord.toUpperCase();
 
 var hiddenSecretWord = "";
 
-for(var i=0; i<secretWord.length;++i){
-  if(secretWord[i] === " ")hiddenSecretWord+=" ";
-  else hiddenSecretWord +="-";
+for (var i = 0; i < secretWord.length; ++i) {
+    if (secretWord[i] === " ") hiddenSecretWord += " ";
+    else hiddenSecretWord += "-";
 }
 
-function displaySecretWord(){
-  $("#word-to-guess").html(hiddenSecretWord);
+function displaySecretWord() {
+    $("#word-to-guess").html(hiddenSecretWord);
 }
 
-function startGame(){
-  var alphabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż".toUpperCase();
-  var content='';
-  for(var i=0; i<6; ++i){
-    content+='<div class="row letters-row" ">\n';
-    for(var  j=0; j<6; ++j){
-      if(i==5 && j==5)break;
-      content+='<div class="col-xs-2">\n<div row><div class="col-xs-2"></div><div class="letter btn btn-info center-block" id="letter'+(i*6+j)+'">'+alphabet[i*6+j]+'</div><div class="col-xs-2"></div></div></div>';
+function startGame() {
+    var alphabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż".toUpperCase();
+    var content = '';
+    for (var i = 0; i < 6; ++i) {
+        content += '<div class="row letters-row" ">\n';
+        for (var j = 0; j < 6; ++j) {
+            if (i == 5 && j == 5) break;
+            content += '<div class="col-xs-2">\n<div row><div class="col-xs-2"></div><div class="letter btn btn-info center-block" id="letter' + (i * 6 + j) + '">' + alphabet[i * 6 + j] + '</div><div class="col-xs-2"></div></div></div>';
+        }
+        content += '\n</div>';
     }
-  content+='\n</div>';
-  }
 
-  $("#alphabet").html(content);
+    $("#alphabet").html(content);
 }
 
 
+function handleLetterClick(letter, obj) {
+    var isCorrect = false;
+    for (var i = 0; i < secretWord.length; ++i) {
+        if (secretWord[i] == letter) {
+            hiddenSecretWord = hiddenSecretWord.substr(0,i)+secretWord[i]+ hiddenSecretWord.substr(i+1);
+            isCorrect = true;
+        }
+    }
+    if (isCorrect) obj.addClass("btn-success");
+    else obj.addClass("btn-danger");
+    displaySecretWord();
+}
 
-$(document).ready(function(){
+$(document).ready(function() {
     startGame();
     displaySecretWord();
-    $(".letter").click(function(){
-      $(this).removeClass("btn-info letter");
-      $(this).addClass("btn-danger disabled cursor-normal");
-      //TODO MANAGE GAME LOGIC
+    $(".letter").click(function() {
+        $(this).removeClass("btn-info letter");
+        $(this).addClass("disabled cursor-normal");
+        handleLetterClick($(this).html(), $(this));
+        //TODO MANAGE GAME LOGIC
     });
-}
-);
+});
